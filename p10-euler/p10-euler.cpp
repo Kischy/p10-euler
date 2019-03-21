f6baed57e7a8a1a245fbf8ae2eaddbc43b46288f
Christian Häge <christian.haege@gmx.net>
2019-03-21 17:09:36 +0100

    Added VS project and VS gtest project

---
+++ p10-euler/p10-euler.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+
+
+int main(int argc, char **argv)
+{
+	unsigned int p10_answ = 0;
+
+
+	std::cout << "The answer to the 10th problem of ProjectEuler.Net is " << p10_answ << ".\n";
+
+
+	return 0;
+}
